a223003144fe5610bcc7ebdc192806375bd98e2f
Zandile Buthelezi <155078684+zandy-zee2021@users.noreply.github.com>
2024-04-10 22:07:53 +0200

    Add files via upload

--- webisite.js
+++ webisite.js
@@ -8,3 +8,16 @@ document.addEventListener("DOMContentLoaded", function() {
     });
   });
 });
+
+
+  document.getElementById('downloadButton').addEventListener('click', function() {
+    // Code to download the CV
+    // Replace 'path_to_your_cv' with the actual path to your CV file
+    var cvUrl = 'path_to_your_cv';
+    var link = document.createElement('a');
+    link.href = cvUrl;
+    link.download = 'your_cv_filename.pdf'; // Set the filename for the downloaded CV
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  });
