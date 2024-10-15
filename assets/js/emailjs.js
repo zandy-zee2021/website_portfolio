ae5a9754fa67c85aa3b7f6bb3d16371b0472ea64
Zandile Buthelezi <zbuthelezi023@student.wethinkcode.co.za>
2024-10-15 19:15:47 +0200

    trying to deploy

---
+++ assets/js/emailjs.js
@@ -0,0 +1,18 @@
+
+  // // Initialize EmailJS with your user ID
+  // emailjs.init("3ik4zHUKIf7erOtqh");
+
+  // document.getElementById('contact-form').addEventListener('submit', function(event) {
+  //   event.preventDefault();
+
+  //   // Send the email using EmailJS
+  //   emailjs.sendForm('service_c9czpow', 'template_lzsz50m', this)
+  //     .then(function(response) {
+  //       console.log('Sent successfully:', response);
+  //       alert('Your message has been sent!');
+  //     }, function(error) {
+  //       console.error('Failed to send:', error);
+  //       alert('Failed to send message. Please try again later.');
+  //     });
+  // });
+
